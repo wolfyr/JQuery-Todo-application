662c309c9474a28d2d3b2012428811d5aa787aec
wolfyr <35544104+wolfyr@users.noreply.github.com>
2018-02-23 16:22:36 +0100

    Add edit todo item function
    Add a function "editItem"
    
    This function allow you to adit an item on the todo list.
    
    In the HTML file I added some CSS to overrule the materialize css library and to add a css tooltip to display the "edit todo item" message.

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 
  $('#addItem').on('click', addItemToList);
  $('#thingsTodo').on('click', '.deleteThisItem', deleteItem);
+ $('#thingsTodo').on('click', '#listItem', editItem);
 
     function addItemToList (){
     var newItem = $('#inputField').val();
 
     $('#thingsTodo').append('<div class="section">'+
-                            '<li class="blue-text text-darken-2">' + newItem +
-                            '<i class="small material-icons deleteThisItem">delete</i></li>'+
+                            '<li id="listItem" class="blue-text text-darken-2 tooltip">' + newItem + '<span class="tooltiptext">Click to edit</span></li>'+
+                            '<i class="small material-icons deleteThisItem">delete</i>'+
                             '<div>'+
                             '<div class="divider"></div>');
     $('#inputField').val("");
@@ -17,6 +18,12 @@ $(document).ready(function(){
   function deleteItem(){
     $(this).closest('.section').remove();
     $(this).closest('.divider').remove();
+  }
+
+  function editItem(){
+    var newTodoInput = prompt("Please edit your todo item");
+    $(this).text(newTodoInput);
+
   }
 
 });
